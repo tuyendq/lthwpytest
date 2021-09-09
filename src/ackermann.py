02c647a9ce2d44deca3cafceda14678d21411653
Tuyen Dang <tuyendq@gmail.com>
2021-09-09 22:08:04 +0700

    Add more assertion tests

--- src/ackermann.py
+++ src/ackermann.py
@@ -1,3 +1,7 @@
 def ackermann(m, n):
     if m == 0:
         return n + 1
+    if m > 0 and n == 0:
+        return ackermann(m - 1, 1)
+    if m > 0 and n > 0:
+        return ackermann(m - 1, ackermann(m, n - 1))
--- src/test_ackermann.py
+++ src/test_ackermann.py
@@ -3,3 +3,20 @@ def test_ackermann():
     assert ackermann(0, 0) == 1
     assert ackermann(0, 1) == 2
     assert ackermann(0, 2) == 3
+    assert ackermann(0, 3) == 4
+    assert ackermann(0, 4) == 5
+    assert ackermann(1, 0) == 2
+    assert ackermann(1, 1) == 3
+    assert ackermann(1, 2) == 4
+    assert ackermann(1, 3) == 5
+    assert ackermann(1, 4) == 6
+    assert ackermann(2, 0) == 3
+    assert ackermann(2, 1) == 5
+    assert ackermann(2, 2) == 7
+    assert ackermann(2, 3) == 9
+    assert ackermann(2, 4) == 11
+    assert ackermann(3, 0) == 5
+    assert ackermann(3, 1) == 13
+    assert ackermann(3, 2) == 29
+    assert ackermann(3, 3) == 61
+    assert ackermann(3, 4) == 125
